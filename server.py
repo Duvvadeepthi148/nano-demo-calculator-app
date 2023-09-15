b53abea7f7825fb1ab73d021bcb359d06f2a3eaa
Duvva Deepthi <duvva.deepthi98@gmail.com>
2023-09-15 22:24:22 +0530

    first commit

--- server.py
+++ server.py
@@ -1,19 +1,27 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
-app = Flask(__name__)
+app = Flask(_name_)
 
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return 'Hello world!'
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    numbers = request.get_json()
+    f= numbers["first"]
+    s= numbers["second"]
+    res= f+s
+    return jsonify({"result": res}), 200
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    numbers = request.get_json()
+    f= numbers["first"]
+    s= numbers["second"]
+    res= f-s
+    return jsonify({"result": res}), 200
 
-if __name__ == '__main__':
+if _name_ == '_main_':
     app.run(port=8080,host='0.0.0.0')
